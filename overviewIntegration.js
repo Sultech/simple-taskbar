c2ce4bc14b3f8571811da12194315c69c8401fdf
Sultech <30802163+Sultech@users.noreply.github.com>
2026-08-12 08:17:06 +0100

    revert 37178090c60179fa5246b224e1e4d042cab468e8

--- overviewIntegration.js
+++ overviewIntegration.js
@@ -39,7 +39,6 @@ export class OverviewIntegration {
         this._tracker = Shell.WindowTracker.get_default();
         this._startupState = null;
         this._startupOverviewId = 0;
-        this._overviewWorkspaceRebuildId = 0;
         this._maximizedWindowDrag = null;
     }
 
@@ -59,11 +58,6 @@ export class OverviewIntegration {
             this._settings,
             'changed::panel-autohide-enabled',
             () => this.queueRelayout()
-        );
-        this._connect(
-            Main.layoutManager,
-            'monitors-changed',
-            () => this._queueOverviewWorkspaceRebuild()
         );
         this._connect(
             Main.overview,
@@ -129,36 +123,6 @@ export class OverviewIntegration {
         Main.overview._overview?._controls?.queue_relayout();
     }
 
-    _queueOverviewWorkspaceRebuild() {
-        if (this._overviewWorkspaceRebuildId ||
-            !Main.overview.visibleTarget) {
-            return;
-        }
-
-        this._overviewWorkspaceRebuildId = GLib.idle_add(
-            GLib.PRIORITY_DEFAULT_IDLE,
-            () => {
-                this._overviewWorkspaceRebuildId = 0;
-                if (!Main.overview.visibleTarget)
-                    return GLib.SOURCE_REMOVE;
-
-                const workspacesDisplay =
-                    Main.overview._overview._controls._workspacesDisplay;
-                workspacesDisplay._updateWorkspacesViews();
-                this.queueRelayout();
-                return GLib.SOURCE_REMOVE;
-            }
-        );
-    }
-
-    _cancelOverviewWorkspaceRebuild() {
-        if (!this._overviewWorkspaceRebuildId)
-            return;
-
-        GLib.Source.remove(this._overviewWorkspaceRebuildId);
-        this._overviewWorkspaceRebuildId = 0;
-    }
-
     showAppWindows(app) {
         const overviewShown = Main.overview._shown ?? Main.overview.visible;
         if (this._spreadApp === app) {
@@ -193,7 +157,6 @@ export class OverviewIntegration {
             !this._settings.get_boolean('default-gnome-panel')
         );
         this._cancelStartupOverview();
-        this._cancelOverviewWorkspaceRebuild();
         this._cancelDashVisibilityRepair();
         // If the extension is disabled while the spread is visible, rebuild
         // the live Overview after restoring GNOME's normal window filter.
