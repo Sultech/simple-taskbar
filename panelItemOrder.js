d25cd49db149fd85c7eaf97dc1a57e747a304978
Sultech <30802163+Sultech@users.noreply.github.com>
2026-08-13 08:17:27 +0100

    Revert "Fix detached panel actor theme warnings"

--- panelItemOrder.js
+++ panelItemOrder.js
@@ -119,10 +119,8 @@ export function placePanelItems(boxes, items, order) {
 
         managedActors.add(actor);
         const parent = actor.get_parent();
-        if (parent && (!visible || parent !== boxes[position])) {
-            actor.ensure_style();
+        if (parent && (!visible || parent !== boxes[position]))
             parent.remove_child(actor);
-        }
     }
 
     for (const [position, {leading, trailing}] of targets) {
